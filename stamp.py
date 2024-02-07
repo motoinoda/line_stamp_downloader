1dc0a87858ea4f4013e5fb1c2650b7002edfb692
motoinoda <motoinodax@gmail.com>
2024-02-08 01:50:07 +0900

    m

--- stamp.py
+++ stamp.py
@@ -8,117 +8,54 @@ import sys
 import click
 
 
-# args = sys.argv
-# print("ディレクトリが存在しない場合自動で作成されます")
-# if(len(args)!=3):
-#     print("error")
-#     print("実行する際の引数は以下のようにしてください")
-#     print("python stamp.py save_dir line_url")
-#     sys.exit()
-# else:
-#     output_folder=args[1]
-#     website_url=args[2]
+
 
 
 website_url = click.prompt("lineスタンプのURLを入力してください", type=str)
 
-# ディレクトリパスを入力
-
 output_folder = click.prompt("保存先のパスを入力してください，存在しない場合自動で作成されます，./はhomeになります", type=str)
 
-
-# output_folder="./data"
-
-##ほしいスタンプのurl(line公式)
-#動画
-# website_url = "https://store.line.me/stickershop/product/25533563/ja"
-#画像
-# website_url="https://store.line.me/stickershop/product/30130/ja"
 response = requests.get(website_url)
 html_content = response.text
 
 
-##動くスタンプをダウンロードしたい時
 
-# iphone_check=re.findall('iPhone',html_content)
-https_urls_p = re.findall(r"https://stickershop.line-scdn.net/stickershop\S*android\S*",html_content)
-https_urls_a = re.findall(r"https://stickershop.line-scdn.net/stickershop\S*iPhone\S*",html_content)
-https_urls_s = re.findall(r"https://stickershop.line-scdn.net/stickershop\S*m4a\S*",html_content)
 
-# if(len(https_urls_s) != 0 ):
-#     print("音声付きの画像スタンプです，画像のみを保存します")
-#     https_urls = re.findall(r"https://stickershop.line-scdn.net/stickershop\S*iPhone\S*",html_content)
-    
-#     # 抽出したURLを出力
-#     for i, url in enumerate(https_urls):
-#         # anime_url=re.findall("\S+animation\S+",url)
-#         # print(url)
+pre = re.findall(r"data-preview='{(.*?)}'",html_content)
+if(len(pre)==0):
+    print("urlが間違っています")
+    print("https://store.line.me/home/ja")
+    print("からほしいスタンプを選んでurlをコピーしてください")
+    sys.exit()
 
-#         # if(i%4==0):
-#         url=url[:-11]
-#         print(url)
-#         # 画像をダウンロードして保存
-#         #ディレクトりなければ作る
-#         if not os.path.isdir(output_folder):
-#             os.makedirs(output_folder)
-#         img_name = os.path.basename(url)
-#         img_path = os.path.join(output_folder, str(i))
-#         with open(img_path+".png", 'wb') as img_file:
-#             img_response = requests.get(url)
-#             # print(img_response.content)
-#             img=img_response.content
-#             # print(type(img))
-#             img_file.write(img)
-# print(len(https_urls_a))
-# print(len(https_urls_p))
-# print(len(https_urls_s))
-if(len(https_urls_p)>len(https_urls_a)):
-    print("画像のスタンプです，画像を保存します")
-    https_urls = re.findall(r"https://stickershop.line-scdn.net/stickershop\S*android\S*png\S*",html_content)
-    
-    # 抽出したURLを出力
-    for i, url in enumerate(https_urls):
-        # anime_url=re.findall("\S+animation\S+",url)
-        # print(url)
+if not os.path.isdir(output_folder):
+    os.makedirs(output_folder)
 
-        if(i%4==0):
-            url=url[:-11]
-            print(url)
-            # 画像をダウンロードして保存
-            #ディレクトりなければ作る
-            if not os.path.isdir(output_folder):
-                os.makedirs(output_folder)
-            img_name = os.path.basename(url)
-            img_path = os.path.join(output_folder, str(i))
-            with open(img_path+".png", 'wb') as img_file:
-                img_response = requests.get(url)
-                # print(img_response.content)
-                img=img_response.content
-                # print(type(img))
-                img_file.write(img)
-
-else:
-    print("動画のスタンプです")
-    https_urls = re.findall(r"https://stickershop.line-scdn.net/stickershop\S*iPhone\S*animation\S*",html_content)
-   
-    # 抽出したURLを出力
-    for i, url in enumerate(https_urls):
-        # anime_url=re.findall("\S+animation\S+",url)
-        # print(url)
-        url=url[:-11]
-        print(url)
-
-        #ディレクトりなければ作る
-        if not os.path.isdir(output_folder):
-            os.makedirs(output_folder)
-
+type_dict={'static':'staticUrl','animation':'animationUrl','animation_sound':'animationUrl','popup':'popupUrl','sound':'staticUrl'}
+for k in range(1,len(pre)):
+    input_text=pre[k]
+    # print(pre)
+    pattern_type = r'&quot;([^&]*)&quot; :'
+    pattern_body = r': &quot;([^&]*)&quot;'
+    types = re.findall(pattern_type, input_text)
+    body = re.findall(pattern_body, input_text)
+    typedef = dict(zip(types,body))
+    data_type = type_dict[typedef['type']]
+    url = typedef[data_type]
+    if(data_type=='staticUrl'):
         img_name = os.path.basename(url)
-        img_path = os.path.join(output_folder, str(i))
+        img_path = os.path.join(output_folder, str(k))
         with open(img_path+".png", 'wb') as img_file:
             img_response = requests.get(url)
-            # print(img_response.content)
             img=img_response.content
-            # print(type(img))
+            img_file.write(img)
+
+    else:
+        img_name = os.path.basename(url)
+        img_path = os.path.join(output_folder, str(k))
+        with open(img_path+".png", 'wb') as img_file:
+            img_response = requests.get(url)
+            img=img_response.content
             img_file.write(img)
 
         images = imageio.mimread(img_path+".png")
@@ -132,7 +69,5 @@ else:
             print("このapngはgif化できませんでした")
             print("apngのみ保存されます")
             print("別なツールでapngをgifに変換してください")
-
-
-
+    print(k,"枚保存しました")
 
